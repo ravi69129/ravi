187a4430db4dacd52f2ffbb485a95e72d69705be
fis <fis@fiss-mac-2.local>
2016-05-07 03:14:26 -0700

    Initial Commit

---
+++ ParseData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseData
+//
+//  Created by fis on 07/05/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParseData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseData
+//
+//  Created by fis on 07/05/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
